2e5fc604ab863a0f78067cd47cac24733d9ef604
LeoPap <81252855+LeoPap@users.noreply.github.com>
2023-01-02 16:45:31 +0200

    Add files via upload

--- Greedy.java
+++ Greedy.java
@@ -1,47 +1,39 @@
-import java.io.BufferedInputStream;
-import java.io.File;
+
+import edu.aueb.ds.exceptions.EmptyListException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Greedy {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, EmptyListException {
         // A list containing the files' spaces in MB
-        int[] MB;
+        List<Integer> MB = new List<>();
         // create new file object
         FileInputStream file = new FileInputStream(args[0]);
 
         Scanner sc = new Scanner(file); // create a new scanner object
         Disk disk = new Disk(); // create a new disk object
-        int counter = 0; // initialize counter
+        int sumOfAllFolders = 0;
         // read each line and
         // count number of lines
-        while (sc.hasNextLine()) {
-            sc.nextLine();
-            counter++;
+        while (sc.hasNext()) {
+            MB.insertAtFront(sc.nextInt());
+            int fileMB = MB.removeFromFront();
+            sumOfAllFolders += fileMB;
+            if (fileMB < 0 && fileMB > 1000000) {
+                if (disk.getFreeSpace() >= fileMB) {
+
+                }
+            }
 
         }
-        file = new FileInputStream(args[0]);
-
-        BufferedInputStream input = new BufferedInputStream(file);
-        MB = new int[counter];
-        for (int i = 0; i < MB.length; i++) {
-            MB[i] = input.read();
-        }
-        System.out.println("Total Number of Lines: " + counter);
-        System.out.println(MB.length);
-        System.out.println(MB[0]);
-        System.out.println(MB[1]);
-        System.out.println(MB[2]);
-        System.out.println(MB[3]);
-        System.out.println(MB[4]);
-        System.out.println(MB[5]);
-        disk.folders.enqueue(MB[1]);
-        System.out.println(disk.getFreeSpace());
-        // close scanner
+        // System.out.println(MB.length);
+        // close scanner}
         sc.close();
-
+        float i = sumOfAllFolders;
+        System.out.println("Sum of all folders = " + i / 1000000 + " TB");
+        System.out.println("Total number of disks used = ");
     }
 
 }
